8e2ab9af44018f5a2e323aeab01e44516ae43693
blaise2s <blaise.schaeffer@gmail.com>
2024-10-04 09:26:46 -0500

    chore: add support for prettier-plugin-tailwindcss

--- .pnp.cjs
+++ .pnp.cjs
@@ -41,6 +41,7 @@ const RAW_RUNTIME_STATE =
           ["lint-staged", "npm:15.2.10"],\
           ["postcss", "npm:8.4.47"],\
           ["prettier", "npm:3.3.3"],\
+          ["prettier-plugin-tailwindcss", "virtual:9d38c80dd9b6be39c8c3c7b371dd21553676cc93f1c064c684c0de00920d431ee406e2f609cee179e9c4003b7a6889c73b4a2685ccd6633072ef9b911cbe9bd3#npm:0.6.8"],\
           ["react", "npm:18.3.1"],\
           ["react-dom", "virtual:9d38c80dd9b6be39c8c3c7b371dd21553676cc93f1c064c684c0de00920d431ee406e2f609cee179e9c4003b7a6889c73b4a2685ccd6633072ef9b911cbe9bd3#npm:18.3.1"],\
           ["stylelint", "npm:16.9.0"],\
@@ -3812,6 +3813,92 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["prettier-plugin-tailwindcss", [\
+      ["npm:0.6.8", {\
+        "packageLocation": "../../../../.yarn/berry/cache/prettier-plugin-tailwindcss-npm-0.6.8-6619c11dc8-10c0.zip/node_modules/prettier-plugin-tailwindcss/",\
+        "packageDependencies": [\
+          ["prettier-plugin-tailwindcss", "npm:0.6.8"]\
+        ],\
+        "linkType": "SOFT"\
+      }],\
+      ["virtual:9d38c80dd9b6be39c8c3c7b371dd21553676cc93f1c064c684c0de00920d431ee406e2f609cee179e9c4003b7a6889c73b4a2685ccd6633072ef9b911cbe9bd3#npm:0.6.8", {\
+        "packageLocation": "./.yarn/__virtual__/prettier-plugin-tailwindcss-virtual-bf1f6ac185/5/.yarn/berry/cache/prettier-plugin-tailwindcss-npm-0.6.8-6619c11dc8-10c0.zip/node_modules/prettier-plugin-tailwindcss/",\
+        "packageDependencies": [\
+          ["prettier-plugin-tailwindcss", "virtual:9d38c80dd9b6be39c8c3c7b371dd21553676cc93f1c064c684c0de00920d431ee406e2f609cee179e9c4003b7a6889c73b4a2685ccd6633072ef9b911cbe9bd3#npm:0.6.8"],\
+          ["@ianvs/prettier-plugin-sort-imports", null],\
+          ["@prettier/plugin-pug", null],\
+          ["@shopify/prettier-plugin-liquid", null],\
+          ["@trivago/prettier-plugin-sort-imports", null],\
+          ["@types/ianvs__prettier-plugin-sort-imports", null],\
+          ["@types/prettier", null],\
+          ["@types/prettier-plugin-astro", null],\
+          ["@types/prettier-plugin-css-order", null],\
+          ["@types/prettier-plugin-import-sort", null],\
+          ["@types/prettier-plugin-jsdoc", null],\
+          ["@types/prettier-plugin-marko", null],\
+          ["@types/prettier-plugin-multiline-arrays", null],\
+          ["@types/prettier-plugin-organize-attributes", null],\
+          ["@types/prettier-plugin-organize-imports", null],\
+          ["@types/prettier-plugin-sort-imports", null],\
+          ["@types/prettier-plugin-style-order", null],\
+          ["@types/prettier-plugin-svelte", null],\
+          ["@types/prettier__plugin-pug", null],\
+          ["@types/shopify__prettier-plugin-liquid", null],\
+          ["@types/trivago__prettier-plugin-sort-imports", null],\
+          ["@types/zackad__prettier-plugin-twig-melody", null],\
+          ["@zackad/prettier-plugin-twig-melody", null],\
+          ["prettier", "npm:3.3.3"],\
+          ["prettier-plugin-astro", null],\
+          ["prettier-plugin-css-order", null],\
+          ["prettier-plugin-import-sort", null],\
+          ["prettier-plugin-jsdoc", null],\
+          ["prettier-plugin-marko", null],\
+          ["prettier-plugin-multiline-arrays", null],\
+          ["prettier-plugin-organize-attributes", null],\
+          ["prettier-plugin-organize-imports", null],\
+          ["prettier-plugin-sort-imports", null],\
+          ["prettier-plugin-style-order", null],\
+          ["prettier-plugin-svelte", null]\
+        ],\
+        "packagePeers": [\
+          "@ianvs/prettier-plugin-sort-imports",\
+          "@prettier/plugin-pug",\
+          "@shopify/prettier-plugin-liquid",\
+          "@trivago/prettier-plugin-sort-imports",\
+          "@types/ianvs__prettier-plugin-sort-imports",\
+          "@types/prettier-plugin-astro",\
+          "@types/prettier-plugin-css-order",\
+          "@types/prettier-plugin-import-sort",\
+          "@types/prettier-plugin-jsdoc",\
+          "@types/prettier-plugin-marko",\
+          "@types/prettier-plugin-multiline-arrays",\
+          "@types/prettier-plugin-organize-attributes",\
+          "@types/prettier-plugin-organize-imports",\
+          "@types/prettier-plugin-sort-imports",\
+          "@types/prettier-plugin-style-order",\
+          "@types/prettier-plugin-svelte",\
+          "@types/prettier",\
+          "@types/prettier__plugin-pug",\
+          "@types/shopify__prettier-plugin-liquid",\
+          "@types/trivago__prettier-plugin-sort-imports",\
+          "@types/zackad__prettier-plugin-twig-melody",\
+          "@zackad/prettier-plugin-twig-melody",\
+          "prettier-plugin-astro",\
+          "prettier-plugin-css-order",\
+          "prettier-plugin-import-sort",\
+          "prettier-plugin-jsdoc",\
+          "prettier-plugin-marko",\
+          "prettier-plugin-multiline-arrays",\
+          "prettier-plugin-organize-attributes",\
+          "prettier-plugin-organize-imports",\
+          "prettier-plugin-sort-imports",\
+          "prettier-plugin-style-order",\
+          "prettier-plugin-svelte",\
+          "prettier"\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["proc-log", [\
       ["npm:4.2.0", {\
         "packageLocation": "../../../../.yarn/berry/cache/proc-log-npm-4.2.0-4d65296a9d-10c0.zip/node_modules/proc-log/",\
@@ -3998,6 +4085,7 @@ const RAW_RUNTIME_STATE =
           ["lint-staged", "npm:15.2.10"],\
           ["postcss", "npm:8.4.47"],\
           ["prettier", "npm:3.3.3"],\
+          ["prettier-plugin-tailwindcss", "virtual:9d38c80dd9b6be39c8c3c7b371dd21553676cc93f1c064c684c0de00920d431ee406e2f609cee179e9c4003b7a6889c73b4a2685ccd6633072ef9b911cbe9bd3#npm:0.6.8"],\
           ["react", "npm:18.3.1"],\
           ["react-dom", "virtual:9d38c80dd9b6be39c8c3c7b371dd21553676cc93f1c064c684c0de00920d431ee406e2f609cee179e9c4003b7a6889c73b4a2685ccd6633072ef9b911cbe9bd3#npm:18.3.1"],\
           ["stylelint", "npm:16.9.0"],\

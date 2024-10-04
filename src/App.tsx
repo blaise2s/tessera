a187d7fe131c1a8246709709ce122b4b2fc5df6b
blaise2s <blaise.schaeffer@gmail.com>
2024-10-04 09:52:46 -0500

    chore: remove comments about importing from pub or assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// How to import from assets and public folders
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-
 export const App = () => {
   return <div>Release of Information</div>;
 };
